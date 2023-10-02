1b462c997cf923540b6b22ba866b402e750307f8
bruhcheslav <bitvull@gmail.com>
2023-10-02 21:58:38 +0400

    запускается на Android, но не удалось решить проблему с ошибками в IDE в файлах Android

---
+++ android/app/src/main/kotlin/io/bruhfessional/tictactoe/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.bruhfessional.tictactoe
+
+import android.os.Bundle
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this));
+    }
+}
